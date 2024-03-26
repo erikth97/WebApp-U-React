4924d8446c92a3c3b72aad04d55d6d30d24d21f3
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-03-25 22:24:28 -0600

    Contact Information & Form

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Contact.css';
+import msg_icon from '../../assets/msg-icon.png';
+import mail_icon from '../../assets/mail-icon.png';
+import phone_icon from '../../assets/phone-icon.png';
+import location_icon from '../../assets/location-icon.png';
+import white_arrow from '../../assets/white-arrow.png'
+
+const Contact = () => {
+    return (
+        <div className='contact'>
+            <div className='contact-col'>
+                <h3>Send Us a Message <img src={msg_icon} alt="" /></h3>
+                <p>Feel free to search out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community</p>
+                <ul>
+                    <li><img src={mail_icon} alt="" />Contact@erikdev.com</li>
+                    <li><img src={phone_icon} alt="" />+52 8182066693</li>
+                    <li><img src={location_icon} alt="" />Monterrey, Nuevo Leon. <br />Mexico</li>
+                </ul>
+            </div>
+                <div className="contact-col">
+                <form>
+                    <label>Your Name</label>
+                    <input type="text" name='name' placeholder='Enter your name' required />
+                    <label>Phone Number</label>
+                    <input type="tel" name='phone' placeholder='Enter your mobile number' required />
+                    <label>Write your message here</label>
+                    <textarea name="messag" rows="6" placeholder='Enter your message' required></textarea>
+                    <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
+                </form>
+                <span>sending... </span>
+                </div>
+        </div>
+    )
+}
+
+export default Contact
